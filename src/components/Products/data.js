94865e1433fa9846c0555b3e8772b4bc324021ed
Miguel Maidana <maidana.miguell@gmail.com>
2020-12-08 20:23:20 -0300

    Agrego Productos

---
+++ src/components/Products/data.js
@@ -0,0 +1,35 @@
+import producto1 from "../../images/VictoriaHogar-26.jpg"
+import producto2 from "../../images/VictoriaHogar-34.jpg"
+import producto3 from "../../images/VictoriaHogar-15.jpg"
+
+export const productData = [
+    {
+        img:producto1,
+        alt : "Cocina",
+        name : "Cocina Multiple",
+        desc :"Cocina con 4 hornallas , plancheta y carlitero",
+        price :"$28800",
+        button : "Encarga la tuya !!"
+
+    },
+    {
+        img:producto2,
+        alt : "Cocina",
+        name : "Cocina 4 Hornallas ",
+        desc :"Cocina con 4 hornallas , piso de ladrillo reflactario",
+        price :"$24600",
+        button : "Encarga la tuya !!"
+
+    },
+    {
+        img:producto3,
+        alt : "Cocina",
+        name : "Cocina 4 y plancha",
+        desc :"Cocina con 4 hornallas , plancheta",
+        price :"$26800",
+        button : "Encarga la tuya !!"
+
+    },
+
+
+]
